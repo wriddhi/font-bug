2dc05e3c1fa7a56a59164796ebf2c47da51c2558
wriddhi <wriddhihazra@gmail.com>
2024-11-14 20:02:19 +0530

    added custom fonts

---
+++ src/config/fonts.ts
@@ -0,0 +1,8 @@
+import { Roboto_Flex } from "next/font/google";
+
+export const roboto = Roboto_Flex({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin-ext"],
+  display: "swap",
+  variable: "--font-roboto",
+});
